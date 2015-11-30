8926cd21cc8cd8a6e8ca5a4a4400435f12f1b135
Stefan Scherer <scherer_stefan@icloud.com>
2015-11-30 18:24:22 +0100

    Create cpuid.ps1

---
+++ cpuid.ps1
@@ -0,0 +1,11 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+wget "http://downloads.sourceforge.net/project/gnuwin32/cpuid/3.3-1/cpuid-3.3-1-bin.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fgnuwin32%2Ffiles%2Fcpuid%2F3.3-1%2Fcpuid-3.3-1-bin.zip%2Fdownload%3Fuse_mirror%3Dskylink%26download%3D&ts=1448903528&use_mirror=netix" -outfile cpuid.zip
+Unzip cpuid.zip "."
+
+. cpuid\bin\cpuid.exe
